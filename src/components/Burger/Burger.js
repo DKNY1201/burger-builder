a15e4ae9b65c3598cd95c695779d83c1118e1dba
Quy Tran <quytran288@gmail.com>
2018-03-21 22:41:22 -0700

    Starting burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,3 +1,15 @@
-/**
- * Created by Bi on 3/21/18.
- */
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => (
+    <div className={classes.Burger}>
+        <BurgerIngredient type="bread-top" />
+        <BurgerIngredient type="salad" />
+        <BurgerIngredient type="bacon" />
+        <BurgerIngredient type="bread-bottom" />
+    </div>
+);
+
+export default burger;
