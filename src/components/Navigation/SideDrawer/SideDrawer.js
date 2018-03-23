e33e53d5a22d5e52074651fce3b24f50141a6c22
Quy Tran <quytran288@gmail.com>
2018-03-23 00:43:03 -0700

    Show/hide Drawer and Backdrop base on state property from Layout

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,16 +3,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Aux from '../../../hoc/Aux';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = (props) => (
-    <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-        <nav>
-            <NavigationItems />
-        </nav>
-    </div>
-);
+const sideDrawer = (props) => {
+    let assignedClass = [classes.SideDrawer]
+    if (props.open) {
+        assignedClass.push(classes.Open);
+    } else {
+        assignedClass.push(classes.Close);
+    }
+
+    return (
+        <Aux>
+            <BackDrop show={props.open} clicked={props.closed} />
+            <div className={assignedClass.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+}
 
 export default sideDrawer;
