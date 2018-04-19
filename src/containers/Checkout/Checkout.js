3bbb464d52522248e984f2a81e4164cc63032ebc
Quy Tran <quytran288@gmail.com>
2018-04-18 22:30:56 -0500

    Redirect to Homepage when no data in checkout summary

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -33,16 +33,26 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}
-                />
-                <Route
+        let summary = <Redirect to="/"/>
+
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}
+                    />
+                    <Route
                     path={this.props.match.path + '/contact-data'}
                     component={ContactData} />
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                {summary}
             </div>
         )
     }
